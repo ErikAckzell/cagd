5d7c163578271f4778407e45e56a9d13b8a35fdf
Erik Ackzell <erik.ackzell@gmail.com>
2016-09-26 14:44:28 +0200

    Finished task 4, added a plot of the spline

--- homework3/bsplines.py
+++ homework3/bsplines.py
@@ -7,16 +7,19 @@ class bspline:
 
     def __init__(self, knots, degree=None, controlpoints=None):
         self.knots = knots
-        
-        if controlpoints:
+
+        if controlpoints is not None:
             self.controlpoints = controlpoints
             self.degree = len(self.knots) - 1 - len(self.controlpoints)
             if degree:
                 assert degree == self.degree, \
-                       'Given degree is wrong. Check the knots and control points or do not define a degree yourself.'
-                
-    def __call__(self, u, degree, controlpoints):
-        S = sum([controlpoints[i] * self.get_basisfunc(k=degree,
+                       'Given degree is wrong. Check the knots and control' + \
+                       'points or do not define a degree yourself.'
+        else:
+            self.degree = degree
+
+    def __call__(self, u):
+        S = sum([controlpoints[i] * self.get_basisfunc(k=self.degree,
                                                        j=i)(u)
                  for i in range(len(controlpoints))])
         return S
@@ -48,12 +51,13 @@ class bspline:
             else:
                 try:
                     a0 = 0 if self.knots[j+k] == self.knots[j] \
-                           else (u - self.knots[j])/(self.knots[j+k]-self.knots[j])
+                           else (u - self.knots[j]) / (self.knots[j+k] -
+                                                       self.knots[j])
                     a1 = 0 if self.knots[j+k+1] == self.knots[j+1] \
-                           else (self.knots[j+k+1] - u)/(self.knots[j+k+1] - self.knots[j+1])
-                    print(a0, a1)
-                    basisfunc = a0 * basisfunction(u, k=k-1, j=j) \
-                                + a1 * basisfunction(u, k=k-1, j=j+1)
+                           else (self.knots[j+k+1] - u) / (self.knots[j+k+1] -
+                                                           self.knots[j+1])
+                    basisfunc = a0 * basisfunction(u, k=k-1, j=j) + \
+                                a1 * basisfunction(u, k=k-1, j=j+1)
                 except IndexError:
                     numBasisfunc = len(self.knots) - 1 - k
                     raise IndexError('Invalid index. There are no more than {} basis functions for the given problem, choose an ' \
@@ -62,31 +66,40 @@ class bspline:
                 return basisfunc
         return basisfunction
 
-    def plot(self, degree):
-        ulist = scipy.linspace(self.knots[0], self.knots[-1], 200)
-        ulist = [u for u in ulist if self.has_full_support(u=u, degree=degree)]
-        print(ulist)
-        pylab.plot(*zip(*[self(u=u, degree=degree) for u in ulist]))
+    def plot(self):
+        ulist = scipy.linspace(self.knots[0], self.knots[-1], 1000)
+        ulist = [u for u in ulist if self.has_full_support(u=u)]
+        pylab.plot(*zip(*[self(u=u) for u in ulist]))
         pylab.plot(*zip(*self.controlpoints), 'o--')
+        pylab.grid()
+        pylab.title('B-spline curve and its control polygon')
         pylab.show()
 
+### Task 2 inspiration###
+#knots = scipy.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+#s = bspline(degree=2, knots=knots)
+#ulist = scipy.linspace(s.knots[0], s.knots[-1], 200)
+#plotlist = [s.get_basisfunc(k=3, j=4)(u) for u in ulist]
+#pylab.plot(ulist, plotlist)
+
+
 ### Task 3 ###
-#knots1 = scipy.array([0, 0, 1, 1])
-#knots2 = scipy.array([0, 0, 0, 1, 1, 1])
-#knots3 = scipy.array([0, 0, 0, 0.3, 0.5, 0.6, 1, 1, 1])
-#
-#degree = 2
-#
-#s1 = bspline(degree, knots=knots1)
-#s2 = bspline(degree=degree, knots=knots2)
-#s3 = bspline(degree=degree, knots=knots3)
-#
-#tlist = scipy.array([0.12, 0.24, 0.4, 0.53, 0.78, 0.8])
-#
-#for i, spline in enumerate([s1, s2, s3]):
-#    print('For spline {}'.format(i+1))
-#    for t in tlist:
-#        print('t={0} : {1}'.format(t, spline.has_full_support(t)))
+knots1 = scipy.array([0, 0, 1, 1])
+knots2 = scipy.array([0, 0, 0, 1, 1, 1])
+knots3 = scipy.array([0, 0, 0, 0.3, 0.5, 0.6, 1, 1, 1])
+
+degree = 2
+
+s1 = bspline(knots=knots1, degree=degree)
+s2 = bspline(knots=knots2, degree=degree)
+s3 = bspline(knots=knots3, degree=degree)
+
+tlist = scipy.array([0.12, 0.24, 0.4, 0.53, 0.78, 0.8])
+
+for i, spline in enumerate([s1, s2, s3]):
+    print('For spline {}'.format(i+1))
+    for t in tlist:
+        print('t={0} : {1}'.format(t, spline.has_full_support(t)))
 
 ### Task 4 ###
 controlpoints = scipy.array([[0, 0],
@@ -97,14 +110,6 @@ controlpoints = scipy.array([[0, 0],
                              [10, -1],
                              [7, -1]])
 
-
-#ulist = scipy.linspace(s3.knots[0], s3.knots[-1], 200)
-#plotlist = [s3.get_basisfunc(k=2, j=1)(u) for u in ulist]
-#pylab.plot(ulist, plotlist)
-
-#knots = scipy.array([0, 0, 0, 0.3, 0.5, 0.5, 0.6, 1, 1, 1])
-knots = scipy.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-s = bspline(degree=2, knots=knots)
-ulist = scipy.linspace(s.knots[0], s.knots[-1], 200)
-plotlist = [s.get_basisfunc(k=3, j=4)(u) for u in ulist]
-pylab.plot(ulist, plotlist)
+knots = scipy.array([0, 0, 0, 0.3, 0.5, 0.5, 0.6, 1, 1, 1])
+s = bspline(knots=knots, controlpoints=controlpoints)
+s.plot()
