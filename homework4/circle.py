e5d9700353eb589647f15f1669534beb58eff5d1
Erik Ackzell <erik.ackzell@gmail.com>
2016-10-17 17:54:30 +0200

    added an illustrative figure.

--- homework4/circle.py
+++ homework4/circle.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  4 12:20:46 2016
+Created on Mon Oct 17 17:40:04 2016
 
 @author: erik
 """
@@ -10,16 +10,17 @@ import scipy
 from matplotlib import pyplot
 
 
-def circle1(t):
-    return (1 / (1 + t ** 2)) * scipy.array([t ** 2 - 1, - 2 * t])
+def circle(u):
+    return scipy.array([scipy.sin(u), scipy.cos(u)])
 
-tlist = scipy.linspace(0, 1, 200)
-plotlist1 = [circle1(t) for t in tlist]
-pyplot.clf()
-pyplot.plot(*zip(*plotlist1), label='circle segment')
-pyplot.xlim([-1, 0])
-pyplot.ylim([-1, 0])
-pyplot.xlabel('x')
-pyplot.ylabel('y')
+
+def y(t, x):
+    return t * (x - 1)
+
+tlist = scipy.linspace(0, 2 * scipy.pi, 200)
+xlist = scipy.linspace(-1, 1, 2)
+for t in [0, 0.25, 0.5, 1]:
+    pyplot.plot(xlist, [y(t, x) for x in xlist])
+pyplot.plot(*zip(*[circle(t) for t in tlist]))
+pyplot.title('Circle and example lines intersecting (1, 0)\nand lower left circle segment')
 pyplot.grid()
-pyplot.legend(loc='best')
