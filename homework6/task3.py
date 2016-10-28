4c27f6239a12e0cb2d81a3c2a08c6868cae5c781
Erik Ackzell <erik.ackzell@gmail.com>
2016-10-28 17:09:04 +0200

    Finished the code for task 3.

--- homework6/task3.py
+++ homework6/task3.py
@@ -16,13 +16,20 @@ def get_2d_grid(datapoints):
     return grid
 
 
-def evaluate_bezier_surface(controlpoints, u, v, m, n):
+def evaluate_bezier_surface(controlpoints, u, v, m=1, n=1):
     M = scipy.zeros((m + 1) * (n + 1))
     for i in range(m + 1):
         for j in range(n + 1):
             M[i * (m + 1) + j] = B(n, i, u) *\
                                  B(n, j, v)
     return M.dot(controlpoints)
+
+
+def plane(coeffs, x, y):
+    a = coeffs[0]
+    b = coeffs[1]
+    c = coeffs[2]
+    return scipy.array([x, y, a * x + b * y + c])
 
 if __name__ == '__main__':
     pyplot.close('all')
@@ -31,27 +38,27 @@ if __name__ == '__main__':
                               [1, 0, 0],
                               [0, 1, 0],
                               [0.5, 0.5, 1]])
-    grid = get_2d_grid(datapoints=datapoints)
-
-    m = 1
-    n = 1
-    K = 4
-
-    M = scipy.zeros((K + 1, (m + 1) * (n + 1)))
-
-    for k in range(K + 1):
-        for i in range(m + 1):
-            for j in range(n + 1):
-                M[k, i * (m + 1) + j] = B(m, i, grid[k, 0]) *\
-                                        B(n, j, grid[k, 1])
-
-    A = M.transpose().dot(M)
-    b = M.transpose().dot(datapoints)
-    controlpoints = scipy.linalg.solve(A, b)
-
-#    degree = 1
     ugrid = scipy.linspace(0, 1, 50)
     vgrid = scipy.copy(ugrid)
+
+    M = scipy.hstack((datapoints[:, :2], scipy.ones((len(datapoints), 1))))
+    tmp = datapoints[:, -1]
+    A = M.transpose().dot(M)
+    b = M.transpose().dot(tmp)
+
+    coeffs = scipy.linalg.solve(A, b)
+
+    xmin = datapoints[:, 0].min()
+    xmax = datapoints[:, 0].max()
+    ymin = datapoints[:, 1].min()
+    ymax = datapoints[:, 1].max()
+
+    controlpoints = scipy.zeros((4, 3))
+
+    controlpoints[0] = plane(coeffs, xmin, ymin)
+    controlpoints[1] = plane(coeffs, xmin, ymax)
+    controlpoints[2] = plane(coeffs, xmax, ymin)
+    controlpoints[3] = plane(coeffs, xmax, ymax)
 
     X = scipy.zeros((len(vgrid), len(ugrid)))
     Y = scipy.zeros((len(vgrid), len(ugrid)))
@@ -60,9 +67,7 @@ if __name__ == '__main__':
         for j, u in enumerate(ugrid):
             z = evaluate_bezier_surface(controlpoints=controlpoints,
                                         u=u,
-                                        v=v,
-                                        m=m,
-                                        n=n)
+                                        v=v)
             X[i, j] = z[0]
             Y[i, j] = z[1]
             Z[i, j] = z[2]
@@ -70,4 +75,8 @@ if __name__ == '__main__':
     figure = pyplot.figure()
     axis = pyplot.subplot(111, projection='3d')
     axis.plot_wireframe(X, Y, Z)
+    for p in datapoints:
+        axis.scatter(p[0], p[1], p[2], c='r', marker='o')
+    for c in controlpoints:
+        axis.scatter(c[0], c[1], c[2], c='b', marker='d')
     pyplot.show()
